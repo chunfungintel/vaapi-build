367d43571e26806b028c6af43869a2ba94c49572
Leow Chun Fung <chun.fung.leow@intel.com>
2022-10-11 10:11:12 +0800

    update

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -5,7 +5,11 @@ IMAGE_TAG=${1//dockerfile_}
 IMAGE_NAME=gar-registry.caas.intel.com/virtiot/vaapi-dxg:${IMAGE_TAG}
 DOCKERFILE=${1}
 
-DOCKER_BUILDKIT=1 docker build -f ${DOCKERFILE} -t $IMAGE_NAME .
+DOCKER_BUILDKIT=1 \
+docker build \
+--cpuset-cpus 0-3 \
+-f ${DOCKERFILE} \
+-t $IMAGE_NAME .
 docker push $IMAGE_NAME
 
 echo $IMAGE_NAME
