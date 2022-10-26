c9727d409f3f2b76184208879a468fae13a66270
Leow Chun Fung <chun.fung.leow@intel.com>
2022-10-26 12:02:05 +0800

    use netrc authentication for github access

--- docker-build/build.sh
+++ docker-build/build.sh
@@ -8,8 +8,11 @@ DOCKERFILE=${1}
 DOCKER_BUILDKIT=1 \
 docker build \
 --cpuset-cpus 0-$(nproc) \
+--secret id=mynetrc,src=$HOME/.netrc \
+--no-cache=false \
 -f ${DOCKERFILE} \
 -t $IMAGE_NAME .
+
 docker push $IMAGE_NAME
 
 echo $IMAGE_NAME
